d3e57e9041d83cd382d3a49811fb60216e245be8
daniny14 <daniny14102004@gmai.com>
2023-08-07 17:59:14 -0300

    tiudo rodando na porta 3k

--- src/server.js
+++ src/server.js
@@ -1,1 +1,11 @@
-console.log("Rodando servidor...")
+const express = require('express')
+
+const api = express()
+
+api.get('/',(req, res) => {
+    res.json({message: "bem vindo a nossa api"})
+})
+
+api.listen(3000, () => {
+    console.log('server rodando na porta 3000. http://localhost:3000')
+})
